672e6afbeeee0000a1720ebcedba56be285ad590
shoaib inamdar <shoaibinamdar14@gmail.com>
2024-04-10 10:28:19 +0530

    music link added

--- lib/pages/music/music_model.dart
+++ lib/pages/music/music_model.dart
@@ -13,26 +13,27 @@ class MusicModel {
 
 List<MusicModel> musics = [
   MusicModel(
-      cover: "https://i.scdn.co/image/ab67616d0000b273b9659e2caa82191d633d6363",
+      cover:
+          "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjJ8fG11c2ljfGVufDB8fDB8fHww",
       author: "Konsta",
       musicName: "O'zbekistonlik",
+      path: 'audio/song1.mp3'),
+  MusicModel(
+      cover:
+          "https://images.unsplash.com/photo-1524678606370-a47ad25cb82a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDR8fG11c2ljfGVufDB8fDB8fHww",
+      author: "Konsta & Timur Alixanov",
+      musicName: "Odamlar nima deydi?",
       path: 'audio/song2.mp3'),
   MusicModel(
       cover:
-          "https://is1-ssl.mzstatic.com/image/thumb/Music112/v4/8c/81/ae/8c81aec1-6bad-ecd2-691b-e0bbcdb2e600/cover.jpg/1200x1200bb.jpg",
-      author: "Konsta & Timur Alixanov",
-      musicName: "Odamlar nima deydi?",
-      path: ''),
-  MusicModel(
-      cover:
-          "https://cdns-images.dzcdn.net/images/cover/7c99f6bb157544db8775430007bb7979/264x264.jpg",
+          "https://images.unsplash.com/photo-1421757350652-9f65a35effc7?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjR8fG11c2ljfGVufDB8fDB8fHww",
       author: "Jon Caryl ",
       musicName: "Alone",
-      path: ''),
+      path: 'audio/song3.mp3'),
   MusicModel(
       cover:
-          "https://is3-ssl.mzstatic.com/image/thumb/Music112/v4/9f/a7/98/9fa798ea-25fc-f447-196a-c9f8bc894669/cover.jpg/600x600bf-60.jpg",
+          "https://images.unsplash.com/photo-1488376739361-ed24c9beb6d0?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Njh8fG11c2ljfGVufDB8fDB8fHww",
       author: "Konsta && Shokir ",
       musicName: "Poyga",
-      path: ''),
+      path: 'audio/song4.mp3'),
 ];
